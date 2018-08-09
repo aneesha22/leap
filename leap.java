a114ab456ea0dd363d20dcae0b3032b42f0cd6b1
aneesha22 <42198319+aneesha22@users.noreply.github.com>
2018-08-09 22:31:04 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int yr;
+             Scanner scan = new Scanner(System.in);
+	 System.out.print("Enter Year:");
+             yr = scan.nextInt();
+             if((yr%4 == 0) && (yr%100!=0))
+             {
+                System.out.print("This is a Leap Year");
+             }
+             else if((yr%100 == 0) && (yr%400 == 0))
+             {
+               System.out.print("This is a Leap Year");
+             }
+             else if(yr%400 == 0)
+             {
+               System.out.print("This is a Leap Year");
+             }
+             else
+             {
+               System.out.print("This is not a Leap Year");
+             }
+        }
+  }
